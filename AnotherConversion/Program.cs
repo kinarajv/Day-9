a469abe26cf3e91e2349e3a29f3372b86e50c53c
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-07-14 08:31:36 +0700

    Add condition on String

--- AnotherConversion/Program.cs
+++ AnotherConversion/Program.cs
@@ -5,28 +5,28 @@ class Program
 	static void Main()
 	{
 		// Parsing hexadecimal string to integer
-		int thirty = Convert.ToInt32("1E", 16); // Parse in hexadecimal (30)
-		Console.WriteLine(thirty); // Output: 30
+		int hexToInt = Convert.ToInt32("1E", 16); // Parse in hexadecimal (30)
+		Console.WriteLine(hexToInt); // Output: 30
 
-		// Parsing binary string to unsigned integer
-		uint five = Convert.ToUInt32("101", 2); // Parse in binary (5)
+		// Parsing binary string to integer
+		int five = Convert.ToInt32("101", 2); // Parse in binary (5)
 		Console.WriteLine(five); // Output: 5
 
 		// Using dynamic data type for implicit conversion
-		dynamic x = 123;
-		dynamic y = "456";
+		dynamic myInt = 123;
+		dynamic myString = "456";
+		
+		// Convert int to a string
+		myString = myInt.ToString();
+		Console.WriteLine(myString); // Output: "123"
 
-		// Convert x to a string
-		string xStr = (string)x;
-		Console.WriteLine(xStr); // Output: "123"
+		// Convert string to an integer
+		bool convertStringtoInt = int.TryParse(myString, out int intParse);
+		Console.WriteLine(intParse + " " + convertStringtoInt); // Output: 456 true
 
-		// Convert y to an integer
-		int yInt = (int)y;
-		Console.WriteLine(yInt); // Output: 456
-
-		// Convert yInt to a double
-		double yDbl = (double)yInt;
-		Console.WriteLine(yDbl); // Output: 456.0
+		// Convert int to a double
+		double  myDouble = intParse;
+		Console.WriteLine(myDouble); // Output: 123.0
 
 		// Converting data types using Convert.ChangeType method
 		Type targetType = typeof(int);
@@ -34,19 +34,22 @@ class Program
 		object result = Convert.ChangeType(source, targetType);
 		Console.WriteLine(result); // Output: 42
 		Console.WriteLine(result.GetType()); // Output: System.Int32
+		
 
 		// Converting boolean to string and back using XmlConvert
 		string s = XmlConvert.ToString(true); // s = "true"
-		bool isTrue = XmlConvert.ToBoolean(s);
+		bool isTrue = XmlConvert.ToBoolean("true");
 		Console.WriteLine(isTrue); // Output: True
 
 		// Converting double to byte array using BitConverter
-		byte[] bytes = BitConverter.GetBytes(123.456);
-		foreach (byte b in bytes)
+		byte[] bytes = new byte[8];
+		bytes = BitConverter.GetBytes(123.456);
+		foreach (var b in bytes)
 		{
 			Console.Write(b + " "); // Output: 0 0 0 0 0 0 94 64
 		}
-
+		Console.WriteLine("");
+		
 		// Converting byte array back to double
 		double dblValue = BitConverter.ToDouble(bytes, 0);
 		Console.WriteLine(dblValue); // Output: 123.456
--- DateTimeSpan/Program.cs
+++ DateTimeSpan/Program.cs
@@ -2,47 +2,57 @@
 
 class Program
 {
-    static void Main()
-    {
-        // DateTime example
-        DateTime dt1 = new DateTime(2022, 4, 1, 9, 0, 0);
-        DateTime dt2 = new DateTime(2022, 4, 1, 14, 0, 0);
+	static void Main()
+	{
+		// DateTime example
+		DateTime date1 = new DateTime(2022, 4, 1, 9, 0, 0);
+		DateTime date2 = new DateTime(2022, 4, 1, 8, 0, 0);
 
-        // Calculate the difference between the two DateTime objects
-        TimeSpan ts = dt2 - dt1;
+		// Calculate the difference between the two DateTime objects
+		TimeSpan timeDiff = date2 - date1;
 
-        // Display the total number of minutes in the TimeSpan object
-        Console.WriteLine("Total minutes: {0}", ts.TotalMinutes);
+		// Display the total number of minutes in the TimeSpan object
+		Console.WriteLine("Total minutes: {0}", timeDiff.TotalMinutes);
 
-        // DateTimeOffset example
-        DateTimeOffset dto1 = new DateTimeOffset(2022, 4, 1, 9, 0, 0, new TimeSpan(-5, 0, 0));
-        DateTimeOffset dto2 = new DateTimeOffset(2022, 4, 1, 14, 0, 0, new TimeSpan(-4, 0, 0));
+		// DateTimeOffset example
+		DateTimeOffset wib = new DateTimeOffset(2022, 4, 1, 9, 0, 0, new TimeSpan(7, 0, 0));
+		DateTimeOffset wita = new DateTimeOffset(2022, 4, 1, 9, 0, 0, new TimeSpan(8, 0, 0));
 
-        // Calculate the difference between the two DateTimeOffset objects
-        TimeSpan ts2 = dto2 - dto1;
+		// Calculate the difference between the two DateTimeOffset objects
+		TimeSpan timeDiff2 = wib - wita;
 
-        // Display the total number of minutes in the TimeSpan object
-        Console.WriteLine("Total minutes: {0}", ts2.TotalMinutes);
+		// Display the total number of minutes in the TimeSpan object
+		Console.WriteLine("Total Hours: {0}", timeDiff2.TotalHours);
 
-        // TimeZoneInfo example
-        TimeZoneInfo tz1 = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-        DateTimeOffset dto3 = TimeZoneInfo.ConvertTime(dto1, tz1);
+		// TimeZoneInfo example
+		TimeZoneInfo tz1 = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+		DateTimeOffset dto3 = TimeZoneInfo.ConvertTime(wib, tz1);
 
-        // Display the converted DateTimeOffset object
-        Console.WriteLine("Converted DateTimeOffset object: {0}", dto3);
+		// Display the converted DateTimeOffset object
+		Console.WriteLine("Before convert DateTimeOffset object: {0}", wib);
+		Console.WriteLine("Converted DateTimeOffset object: {0}", dto3);
 
-        // DateOnly example
-        DateOnly date1 = new DateOnly(2022, 4, 1);
-        DateOnly date2 = date1.AddDays(10);
+		// DateOnly example
+		DateOnly date3 = new DateOnly(2022, 4, 1);
+		DateOnly date4 = date3.AddDays(10);
 
-        // Display the new DateOnly object
-        Console.WriteLine("New DateOnly object: {0}", date2);
+		// Display the new DateOnly object
+		Console.WriteLine("New DateOnly object: {0}", date4);
 
-        // TimeOnly example
-        TimeOnly time1 = new TimeOnly(9, 0, 0);
-        TimeOnly time2 = time1.AddMinutes(30);
+		// TimeOnly example
+		TimeOnly time1 = new TimeOnly(9, 0, 0);
+		TimeOnly time2 = time1.AddMinutes(30);
 
-        // Display the new TimeOnly object
-        Console.WriteLine("New TimeOnly object: {0}", time2);
-    }
+		// Display the new TimeOnly object
+		Console.WriteLine("New TimeOnly object: {0}", time2);
+
+		TimeOnly time3 = new TimeOnly(10, 0, 0);
+		TimeOnly time4 = new TimeOnly(14, 30, 0);
+
+		DateTime dateTime1 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, time1.Hour, time1.Minute, time1.Second);
+		DateTime dateTime2 = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, time2.Hour, time2.Minute, time2.Second);
+
+		TimeSpan duration = dateTime2 - dateTime1;
+
+	}
 }
